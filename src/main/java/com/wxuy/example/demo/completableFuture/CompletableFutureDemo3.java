9edecaf8f0fd504dccc3fb4344486d0add8f1f68
wangxy01 <834791564@qq.com>
2021-04-26 11:24:23 +0800

    利用了查询当前线程堆栈中的信息辨别该代码的调用顺序StackTraceElement

--- src/main/java/com/wxuy/example/demo/completableFuture/CompletableFutureDemo3.java
+++ src/main/java/com/wxuy/example/demo/completableFuture/CompletableFutureDemo3.java
@@ -1,22 +1,39 @@
 package com.wxuy.example.demo.completableFuture;
 
 
+import javax.xml.crypto.Data;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class CompletableFutureDemo3 {
-	public static void main(String[] args) throws ExecutionException, InterruptedException {
+	public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+		System.out.println("程序开始");
 		CompletableFuture<String> supplyAsync = CompletableFuture.supplyAsync(() -> {
 			try {
-				TimeUnit.SECONDS.sleep(1);
+				TimeUnit.SECONDS.sleep(10);
 			} catch (InterruptedException e) {
 				throw new IllegalStateException(e);
 			}
 			return "Result of the asynchronous computation";
 		});
-		String s = supplyAsync.get();
-		System.out.println(s);
+		TimeUnit.SECONDS.sleep(2);
+		System.out.println("supplyAsync 定义完成");
+		CompletableFuture<String> supplyAsync2 = CompletableFuture.supplyAsync(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(2);
+				System.out.println("supplyAsync2 end");
+			} catch (InterruptedException e) {
+				throw new IllegalStateException(e);
+			}
+			return "Result of the asynchronous computation";
+		});
+		TimeUnit.SECONDS.sleep(3);
+		System.out.println("supplyAsync2 定义完成");
+
+		System.out.println(supplyAsync.get(5, TimeUnit.SECONDS));
+		System.out.println(supplyAsync2.get(5, TimeUnit.SECONDS));
 
 	}
 
