664528b41c52ae3b262ceeca4f86206e4683e6e8
wangxy01 <834791564@qq.com>
2021-06-04 17:00:37 +0800

    设计模式-创建模式-原型模式

---
+++ src/main/java/com/wxuy/example/designPatterns/creationMode/prototypeMode/test/ApiTest.java
@@ -0,0 +1,16 @@
+package com.wxuy.example.designPatterns.creationMode.prototypeMode.test;
+
+
+import com.wxuy.example.designPatterns.creationMode.prototypeMode.QuestionBankController;
+
+public class ApiTest {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+
+}
