863900a7955e47d4a6fe2e528f883e90c9ad041e
wangxy01 <834791564@qq.com>
2021-06-18 16:02:36 +0800

    设计模式-结构模式-迭代器模式

---
+++ src/main/java/com/wxuy/example/designPatterns/behaviorPattern/iteratorMode/lang/Iterable.java
@@ -0,0 +1,7 @@
+package com.wxuy.example.designPatterns.behaviorPattern.iteratorMode.lang;
+
+public interface Iterable<E> {
+
+    Iterator<E> iterator();
+
+}
---
+++ src/main/java/com/wxuy/example/designPatterns/behaviorPattern/iteratorMode/lang/Iterator.java
@@ -0,0 +1,9 @@
+package com.wxuy.example.designPatterns.behaviorPattern.iteratorMode.lang;
+
+public interface Iterator<E> {
+
+    boolean hasNext();
+
+    E next();
+    
+}
